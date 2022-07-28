2ae12fb13df90e71b46bf819780d4027893f40cd
Weston <rwwille@gmail.com>
2022-07-28 03:34:08 -0700

    edit files

--- myscript.js
+++ myscript.js
@@ -1,3 +1,11 @@
 $(window).on('load', function () {
-    $('.flexslider').flexslider();
+    $('.flexslider').flexslider(
+        {
+            animation: "slide",
+            slideshowSpeed: 1500,
+            pauseOnHover: true,
+            // direction: "vertical",
+            // reverse: true  
+        }
+    );
 });
